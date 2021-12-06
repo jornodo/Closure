319cc2ec89eeab717dda2326497d10704f696a93
Ole Berg <olber@uio.no>
2021-12-06 12:12:12 +0100

    Initial commit

--- src/CachedFib.java
+++ src/CachedFib.java
@@ -9,25 +9,31 @@ public class CachedFib {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        return (n) -> {
-                if (map.containsKey(n)) {
-                    return map.get(n);
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer n) {
+                {
+                    if (map.containsKey(n)) {
+                        return map.get(n);
+                    }
+
+                    if (n < 2) {
+                        System.out.println("Putting " + n + " into map as: " + n);
+                        map.put(n, n);
+                        return n;
+                    }
+                    ;
+
+                    int result = this.apply(n - 1) + this.apply(n - 2);
+                    System.out.println("Putting " + n + " into map as: " + result);
+                    map.put(n, result);
+                    return result;
                 }
-
-                if(n < 2){
-                    System.out.println("Putting " + n + " into map as: " + n);
-                    map.put(n, n);
-                    return n;
-                };
-
-                int result = fib().apply(n - 1) + fib().apply(n - 2);
-                System.out.println("Putting " + n + " into map as: " + result);
-                map.put(n, result);
-                return result;
+            }
         };
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Function<Integer, Integer> cachedFib = fib();
         System.out.println(cachedFib.apply(5));
         /**
@@ -35,19 +41,12 @@ public class CachedFib {
          Putting 1 into map as: 1
          Putting 0 into map as: 0
          Putting 2 into map as: 1
-         Putting 1 into map as: 1
          Putting 3 into map as: 2
-         Putting 1 into map as: 1
-         Putting 0 into map as: 0
-         Putting 2 into map as: 1
          Putting 4 into map as: 3
-         Putting 1 into map as: 1
-         Putting 0 into map as: 0
-         Putting 2 into map as: 1
-         Putting 1 into map as: 1
-         Putting 3 into map as: 2
          Putting 5 into map as: 5
          5
          */
     }
 }
+
+
